6f56d35d90921ed4cba5557349eb0cc09606a79d
Ashwinth Muthukumar <ashwinth.muthukumar.sync@gmail.com>
2023-10-04 15:08:19 +0530

    Added Bootstrap,Checkbox,select,DOB,radio

--- task.js
+++ task.js
@@ -5,12 +5,22 @@ $(document).ready(function () {
         var name = $("#name").val();
         var email = $("#email").val();
         var tel =$('#tel').val();
+        var date = $("#dob").val();
         var occupation = $("#occupation").val();
+        var gender = $("input[name='gender']:checked").val();
+        var language = new Array(); $("input[name='Language']:checked").each(function() {
+			language.push(" " + this.value);
+		});
+        var martial = $("#martial").val (); 
         var displayValues = $("#displayValues");
         displayValues.html("<p><strong>Name:</strong> " + name + "</p>" +
                             "<p><strong>Email:</strong> " + email + "</p>" +
                             "<p><strong>Mobile No. :</strong> " + tel + "</p>" +
-                            "<p><strong>Occupation:</strong> " + occupation + "</p>");
-        $("#inputForm")[0].reset();  
+                            "<P><strong>Date Of Birth :</strong>" + date + "</p>" +
+                            "<p><strong>Occupation:</strong> " + occupation + "</p>" +
+                            "<p><strong>Gender : </strong> " + gender + "</p>" +
+                            "<p><strong>Martial Status : </strong>" + martial + "</p>" +
+                            "<p><strong>Language : </strong>" + language + "</p>");
+        $("#inputForm")[0].reset();    
     });
 });
